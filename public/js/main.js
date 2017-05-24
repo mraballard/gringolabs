415e92c3df7092568fec017200411b2a79f61128
Aaron Ballard <mraballard@gmail.com>
2017-05-23 21:37:58 -0400

    Removed hidden icon labels that were causing icons to jump on hover.

--- public/js/main.js
+++ public/js/main.js
@@ -25,54 +25,54 @@ var UI = {
       console.log("Click!");
     });
     //Hover Events
-    $('#c2go-icon').hover(
-      function() {
-        $('#c2go-label').show();
-        $('.project-div').css({marginTop: "-11px"});
-      },
-      function() {
-        $('#c2go-label').hide();
-        $('.project-div').css({marginTop: "25px"});
-      }
-    );
-    $('#nitpic-icon').hover(
-      function() {
-        $('#nitpic-label').show();
-        $('.project-div').css({marginTop: "-11px"});
-      },
-      function() {
-        $('#nitpic-label').hide();
-        $('.project-div').css({marginTop: "25px"});
-      }
-    );
-    $('#store-icon').hover(
-      function(){
-        $('#store-label').show();
-        $('.project-div').css({marginTop: "-11px"});
-      },
-      function() {
-        $('#store-label').hide();
-        $('.project-div').css({marginTop: "25px"});
-      }
-    );
-    $('#dog-icon').hover(
-      function(){
-        $('#dog-label').show();
-        $('.project-div').css({marginTop: "-11px"});
-      },
-      function() {
-        $('#dog-label').hide();
-        $('.project-div').css({marginTop: "25px"});
-      }
-    );
+    // $('#c2go-icon').hover(
+    //   function() {
+    //     $('#c2go-label').show();
+    //     $('.project-div').css({marginTop: "-11px"});
+    //   },
+    //   function() {
+    //     $('#c2go-label').hide();
+    //     $('.project-div').css({marginTop: "25px"});
+    //   }
+    // );
+    // $('#nitpic-icon').hover(
+    //   function() {
+    //     $('#nitpic-label').show();
+    //     $('.project-div').css({marginTop: "-11px"});
+    //   },
+    //   function() {
+    //     $('#nitpic-label').hide();
+    //     $('.project-div').css({marginTop: "25px"});
+    //   }
+    // );
+    // $('#store-icon').hover(
+    //   function(){
+    //     $('#store-label').show();
+    //     $('.project-div').css({marginTop: "-11px"});
+    //   },
+    //   function() {
+    //     $('#store-label').hide();
+    //     $('.project-div').css({marginTop: "25px"});
+    //   }
+    // );
+    // $('#dog-icon').hover(
+    //   function(){
+    //     $('#dog-label').show();
+    //     $('.project-div').css({marginTop: "-11px"});
+    //   },
+    //   function() {
+    //     $('#dog-label').hide();
+    //     $('.project-div').css({marginTop: "25px"});
+    //   }
+    // );
   },
 
-  hideLabels: function() {
-    $('#c2go-label').hide();
-    $('#nitpic-label').hide();
-    $('#store-label').hide();
-    $('#dog-label').hide();
-  },
+  // hideLabels: function() {
+  //   $('#c2go-label').hide();
+  //   $('#nitpic-label').hide();
+  //   $('#store-label').hide();
+  //   $('#dog-label').hide();
+  // },
 
   hideDivs: function() {
     $('#c2go-div').hide();
@@ -89,7 +89,7 @@ $(document).ready(function() {
   // var $ = jQuery.noConflict();
   UI.setEvenListeners();
   UI.hideDivs();
-  UI.hideLabels();
+  // UI.hideLabels();
   $('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
